2841a21df5a307b53182cf523ccf30d69cd4f953
sprite <251502027@smail.nju.edu.cn>
2026-01-16 19:35:47 +0800

    Fix an error in mathjax.js

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -4,23 +4,16 @@ window.MathJax = {
     displayMath: [["$$", "$$"], ["\\[", "\\]"]],
     processEscapes: true,
     processEnvironments: true,
-    packages: {'[+]': ['ams', 'extpfeil']},
+    packages: {'[+]': ['extpfeil']},
     macros: {
       d: "{\\mathop{}\\!\\mathrm{d}}"
     }
   },
   loader: {
-    load: ['[tex]/ams', '[tex]/extpfeil']
+    load: ['[tex]/extpfeil']
   },
   options: {
     ignoreHtmlClass: "none",
     processHtmlClass: "arithmatex|md-nav__link|md-toc__link"
   }
 };
-
-document$.subscribe(() => { 
-  MathJax.startup.output.clearCache()
-  MathJax.typesetClear()
-  MathJax.texReset()
-  MathJax.typesetPromise()
-})
